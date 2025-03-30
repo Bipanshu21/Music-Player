f45e34633f16c88aab7dc9565f0bc13ae5d2884c
Bipanshu21 <bipanshukumar21@gmail.com>
2025-03-30 12:46:55 +0530

    Coded prototype

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import AudioFiles from "./components/AudioFiles";
 import { audio } from "./audioData";
 import './App.css';
 import { useState } from "react";
+import Player from './components/Player';
 
 const App = () =>{
  
     const [songs, setSongs] = useState(audio);
     
     return(
+     <>
+        <div className="player-main">
+              <Player/>
+        </div>
+
         <div className="app-main">
                {songs.map((song) =>{
                    return(
@@ -15,6 +21,8 @@ const App = () =>{
                    )
                })}
         </div>
+
+        </>
     )
 }
 
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,21 @@
+import { BsPlay, BsPause } from "react-icons/bs";
+import { BiSkipPrevious, BiSkipNext } from "react-icons/bi";
+ 
 const Player = () =>{
     return(
-        <>
-        </>
+        <div className="player-card">
+
+           <h4 className="song-name">Song Name</h4>
+
+           <div className="control-icons">
+                <BiSkipPrevious color="white" size={50} className="icons"></BiSkipPrevious>
+                <BsPlay color="#1769aa" size={50} className="icons"></BsPlay>
+                <BsPause color="#1769aa" size={50} className="icons"></BsPause>
+                <BiSkipNext color="white" size={50} className="icons"></BiSkipNext>
+           </div>
+
+
+        </div>
     )
 }
 
